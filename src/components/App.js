d053d51328b018226ef76db314d505cf4ec98d46
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-10-05 11:23:16 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,28 +5,31 @@ export const App = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState("");
 
-  const getWeather = async () => {
-    const res = await fetch(
+  const getWeather = () => {
+    fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f63a61575aba27041c68e06fd11c9c51`
-    );
-    const data = await res.json();
-    console.log(data);
-    if (data.cod === "404" || data.cod === "400") return;
-    const DATA = {
-      name: data?.name,
-      temp: data?.main?.temp,
-      sky: data?.weather[0]?.description,
-    };
-    setWeather(DATA);
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.cod === "404" || data.cod === "400") return;
+        const DATA = {
+          name: data?.name,
+          temp: data?.main?.temp,
+          sky: data?.weather[0]?.description,
+        };
+        setWeather(DATA);
+      })
+      .catch((e) => {
+        setWeather("");
+        console.error(e); // Log any errors
+      });
   };
 
   useEffect(() => {
-    try {
-      getWeather();
-    } catch (e) {
-      setWeather("");
-    }
+    getWeather();
   }, [city]);
+
   return (
     <div id="main">
       {/* Do not remove the main divasd */}
